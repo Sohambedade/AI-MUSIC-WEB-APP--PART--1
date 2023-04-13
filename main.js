b4b40648f6e50a467d715be2728a7b6bc5754e5d
Sohambedade <97166009+Sohambedade@users.noreply.github.com>
2023-04-13 09:02:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position( 500,500 );
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
